de32889a8c1cae9c6b095eaa525e65dac643ec46
raynormw <tirtawiryaputra@yahoo.com>
2017-05-19 14:30:55 +0700

    fitur edit

--- routes/users.js
+++ routes/users.js
@@ -4,34 +4,100 @@ var db = require('../models')
 const bodyParser = require('body-parser');
 let parseUrlEncoded = bodyParser.urlencoded({extended : false})
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+/* Admin Dashboard */
+router.get('/', function(req, res) {
   res.render('user');
-  // res.render('./partials/menu');
 });
 
-
-router.get('/data-foodcourt', function(req, res, next) {
-  db.Foodcourt.findAll()
+/* --------------- Foodcourt ----------------- */
+/* List */
+router.get('/data-foodcourt', function(req, res) {
+  db.Foodcourt.findAll({
+    order: "id ASC"
+  })
   .then(foodcourts => {
-    res.render('dataFoodcourt',{ foodcourt : foodcourts});
-    //next();
+    res.render('dataFoodcourt', { foodcourt : foodcourts});
   })
   .catch(err => {
     res.send(err);
   })
 });
 
-router.get('/data-resto', function(req, res, next) {
+/* Input */
+router.get('/input-foodcourt', function(req, res) {
+  db.Foodcourt.findAll()
+  .then(foodcourts => {
+    res.render('addFoodcourt', { data : foodcourts});
+  })
+  .catch(err => {
+    res.send(err);
+  })
+});
+
+/* Create */
+router.post('/createFoodcourt', parseUrlEncoded, function(req, res) {
+  db.Foodcourt.create({
+    name     : req.body.name,
+    address  : req.body.address,
+    city     : req.body.city,
+    picture  : req.body.picture || ''
+  })
+  .then(() => {
+    res.redirect('/users');
+  })
+  .catch((err) => {
+    res.redirect(err)
+  })
+})
+
+/* Edit */
+router.get('/edit-foodcourt/:id', function(req, res) {
+  db.Foodcourt.find({
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(foodcourts => {
+    res.render('editFoodcourt', { data : foodcourts});
+  })
+  .catch(err => {
+    res.send(err);
+  })
+});
+
+/* Update */
+router.post('/update-foodcourt/:id', parseUrlEncoded, function(req, res) {
+  db.Foodcourt.update({
+    name     : req.body.name,
+    address  : req.body.address,
+    city     : req.body.city,
+    picture  : req.body.picture || ''
+  }, {
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(() => {
+    res.redirect('/users/data-foodcourt');
+  })
+  .catch((err) => {
+    res.redirect(err)
+  })
+})
+/* --------------- End of Foodcourt ------------ */
+
+
+/* --------------- Resto ----------------- */
+/* List */
+router.get('/data-resto', function(req, res) {
   db.Resto.findAll({
     include: [
       db.Foodcourt
     ],
-    order: 'id DESC'
+    order: 'id ASC'
   })
   .then(restos => {
     res.render('dataResto', { resto : restos});
-    //next()
   })
   .catch(err => {
     console.log(err);
@@ -39,8 +105,84 @@ router.get('/data-resto', function(req, res, next) {
   })
 });
 
+/* Input */
+router.get('/input-resto', function(req, res) {
+  db.Foodcourt.findAll()
+  .then(foodcourts => {
+    console.log(foodcourts);
+    res.render('addResto', { foodcourt : foodcourts});
+  })
+  .catch(err => {
+    res.send(err);
+  })
+});
 
-router.get('/data-menu', function(req, res, next) {
+/* Create */
+router.post('/createResto', parseUrlEncoded, function(req, res) {
+  db.Resto.create({
+    name : req.body.name,
+    category : req.body.category,
+    picture : req.body.picture,
+    id_foodcourt: req.body.id_foodcourt || ''
+  })
+  .then(() => {
+    res.redirect('/users');
+  })
+  .catch((err) => {
+    res.send(err)
+  })
+})
+
+/* Edit */
+router.get('/edit-resto/:id', function(req, res) {
+  db.Resto.find({
+    where : {
+      id : req.params.id
+    },
+    include : [db.Foodcourt]
+  })
+  // .then(restos => {
+  //   res.render('editResto', { data : restos});
+  // })
+  .then(restos => {
+    db.Foodcourt.findAll()
+    .then(foodcourts => {
+      res.render('editResto', {data : restos, foodcourt: foodcourts});
+    })
+    .catch(err => {
+      res.send(err);
+    })
+  })
+  .catch(err => {
+    res.send(err);
+  })
+});
+
+/* Update */
+router.post('/update-resto/:id', parseUrlEncoded, function(req, res) {
+  db.Resto.update({
+    name : req.body.name,
+    category : req.body.category,
+    picture : req.body.picture,
+    id_foodcourt: req.body.id_foodcourt || ''
+  }, {
+    where : {
+      id : req.params.id
+    }
+  })
+  .then(() => {
+    res.redirect('/users/data-resto');
+  })
+  .catch((err) => {
+    res.redirect(err)
+  })
+})
+/* --------------- End of Resto ------------ */
+
+
+/* --------------- Menu ----------------- */
+/* List */
+router.get('/data-menu', function(req, res) {
   db.Menu.findAll({
     include:[
       db.Resto
@@ -56,60 +198,8 @@ router.get('/data-menu', function(req, res, next) {
   })
 });
 
-/*----------------------------------------------------------------------------*/
-
-router.get('/input-foodcourt', function(req, res, next) {
-  db.Foodcourt.findAll()
-  .then(foodcourts => {
-    res.render('addFoodcourt', { data : foodcourts});
-  })
-  .catch(err => {
-    res.send(err);
-  })
-});
-
-
-
-router.post('/createFoodcourt', function(req, res, next) {
-  let name     = req.body.name
-  let address  = req.body.address
-  let city     = req.body.city
-  let picture  = req.body.picture || ''
-  db.Foodcourt.create(req.body)
-  .then(() => {
-    res.redirect('addFoodcourt')
-  })
-  .catch((err) => {
-    res.redirect(err)
-  })
-})
-// -------------------------- untuk ambil data dari foodcourt --------------------------------------- //
-router.get('/input-resto', function(req, res, next) {
-  db.Foodcourt.findAll()
-  .then(foodcourts => {
-    res.render('addResto', { foodcourt : foodcourts});
-  })
-  .catch(err => {
-    res.send(err);
-  })
-});
-//
-
-router.post('/createResto', function(req, res, next) {
-  let name     = req.body.name
-  let category  = req.body.category
-  let picture     = req.body.picture
-  let id_foodcourt  = req.body.id_foodcourt || ''
-  db.Resto.create(req.body)
-  .then(() => {
-    res.redirect('addFoodcourt')
-  })
-  .catch((err) => {
-    res.redirect(err)
-  })
-})
-//-----------------untuk get data dari tabel resto ----------------------
-router.get('/input-menu', function(req, res, next) {
+/* Input */
+router.get('/input-menu', function(req, res) {
   db.Resto.findAll()
   .then(restos => {
     res.render('addMenu', { resto : restos});
@@ -119,24 +209,22 @@ router.get('/input-menu', function(req, res, next) {
   })
 });
 
-
-//
-
-router.post('/createMenu', function(req, res, next) {
-  let name        = req.body.name
-  let price       = req.body.price
-  let tag         = req.body.tag
-  let picture     = req.body.picture
-  let id_resto    = req.body.id_resto
-  db.Menu.create(req.body)
+/* Create */
+router.post('/createMenu', parseUrlEncoded, function(req, res) {
+  db.Menu.create({
+    name : req.body.name,
+    price : req.body.price,
+    tag : req.body.tag,
+    picture : req.body.picture,
+    id_resto : req.body.id_resto
+  })
   .then(() => {
-    res.redirect('addMenu')
+    res.redirect('/users')
   })
   .catch((err) => {
     res.redirect(err)
   })
 })
-
-
+/* --------------- End of Menu ------------ */
 
 module.exports = router;
